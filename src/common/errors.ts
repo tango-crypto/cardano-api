3a398b745cffca99ee143bc989ce920ec184840d
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-02-04 15:29:54 -0300

    remove camel case for API Error status code

--- src/common/errors.ts
+++ src/common/errors.ts
@@ -1,11 +1,22 @@
 import { BadRequestException, HttpException, HttpStatus, NotFoundException } from "@nestjs/common";
 
+
 export class APIError {
 	static notFound(resource: string): HttpException {
-		return new NotFoundException(`${resource} not found`)
+		const notFoundError = {
+			status_code: 404,
+			message: 'Not Found',
+			error: `${resource} not found`
+		}
+		return new NotFoundException(notFoundError);
 	}
 
 	static badRequest(message: string): HttpException {
-		return new BadRequestException(message);
+		const badRequestError = {
+			status_code: 400,
+			message: 'Bad Request',
+			error: message
+		}
+		return new BadRequestException(badRequestError);
 	}
 }
