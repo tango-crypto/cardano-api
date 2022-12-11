2dfc2e805e8ba3474304e063bddbe09a5dbf9a09
Leobel Izquierdo <leobelizquierdo@gmail.com>
2022-12-10 21:24:43 -0300

    throw the same error

--- src/transactions/transactions.service.ts
+++ src/transactions/transactions.service.ts
@@ -398,8 +398,7 @@ export class TransactionsService {
 			return await this.ogmiosService.evaluateTx(server, cborHex, mapUtxos);
 		} catch (err) {
 			console.log('Evaluate Error:', err);
-			let errorMessage = err.isAxiosError && err.response && err.response.data ? err.response.data : err.message;
-			throw APIError.badRequest(errorMessage || err);
+			throw err;
 		}
 	}
 
