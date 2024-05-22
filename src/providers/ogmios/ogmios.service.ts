d8e2fe4263352bbf65d582048a6fee926abdbc27
Leobel Izquierdo <leobelizquierdo@gmail.com>
2024-05-21 23:36:03 -0400

    cleanup ogmios service

--- src/providers/ogmios/ogmios.service.ts
+++ src/providers/ogmios/ogmios.service.ts
@@ -1,51 +1,17 @@
 import { TxId, Utxo } from '@cardano-ogmios/schema';
 import { Inject, Injectable, Logger, LoggerService } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { createInteractionContext, createTxSubmissionClient, InteractionContext, getServerHealth, createConnectionObject, Connection, ConnectionConfig } from '@cardano-ogmios/client';
-import { EvaluationResult, TxSubmissionClient } from '@cardano-ogmios/client/dist/TxSubmission';
 import axios from 'axios';
 
 @Injectable()
 export class OgmiosService {
-  connection: Connection;
-  connectionConfig: ConnectionConfig;
-  context: InteractionContext;
-  submissionClient: TxSubmissionClient;
   constructor(
-    private configService: ConfigService,
-    @Inject(Logger) private readonly logger: LoggerService,
+    private configService: ConfigService
   ) {
     const host = this.configService.get<string>('OGMIOS_HOST') || 'localhost';
     const port = this.configService.get<number>('OGMIOS_PORT') || 1337;
-    const tls = this.configService.get<string>('OGMIOS_TLS') == 'true';
     axios.defaults.baseURL = `http://${host}:${port}`;
-    this.connectionConfig = { host, port, tls };
-    this.connection = createConnectionObject(this.connectionConfig);
-  }
 
-  async connect(): Promise<void> {
-    const isConnected = await this.isConnected();
-    if (isConnected) {
-      console.log('Already connected');
-      return;
-    }
-    this.context = await createInteractionContext(
-      err => {
-        this.context = null;
-        this.logger.error({ context: 'OgmiosServer', message: `Connection error: ${err}` });
-      },
-      (code: number, reason: string) => {
-        this.context = null;
-        this.logger.log({ context: 'OgmiosServer', message: `Connection closed. Code: ${code}, Reason: ${reason}` });
-      },
-      { connection: this.connectionConfig, interactionType: 'LongRunning' }
-    );
-    this.submissionClient = await createTxSubmissionClient(this.context);
-  }
-
-
-  async isConnected(): Promise<boolean> {
-    return !!this.context && ((await getServerHealth({ connection: this.connection })) as any).connectionStatus == "connected";
   }
 
   async submitTx(cbor: string): Promise<TxId> {
